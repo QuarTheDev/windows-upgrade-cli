bacabc195e290fe2ec9bf0b133456aa5757cdcc4
Quar <70469187+QuarTheDev@users.noreply.github.com>
2023-03-20 18:44:46 -0400

    Create exec.ps1

---
+++ exec.ps1
@@ -0,0 +1,1 @@
+Start-Process powershell -ArgumentList "-Command & { Start-Process cmd -Verb RunAs -ArgumentList '/c iwr -useb https://raw.githubusercontent.com/QuarTheDev/windows-upgrade-cli/main/upgrader.ps1 | iex' }"
